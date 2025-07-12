33b7e646b77f1caccc4e2be699c9ea1420d4d17a
AfnanAli537 <afnan.alial537@gmail.com>
2025-07-13 00:11:14 +0300

    Add files via upload

---
+++ equal or not.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int num1, num2;
+    cout << "Enter two integers: ";
+    cin >> num1 >> num2;
+
+    if (num1 == num2) {
+        cout << "Equal" << endl;
+    }
+    else {
+        cout << "Not Equal" << endl;
+    }
+
+    return 0;
+}
+
